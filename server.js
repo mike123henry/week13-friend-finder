67a98002dd9bf89fda7bab29163a53b0a187ce3b
MikeH <mike123.henry@gmail.com>
2016-07-23 12:32:28 -0500

    removed more console.logs

--- server.js
+++ server.js
@@ -3,8 +3,7 @@
 var express = require("express");
 var app = express();
 app.set("port", process.env.PORT || 3000);
-console.log("express = "+express);
-console.log("app = "+app);
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
