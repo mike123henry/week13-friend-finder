da024f07f114c26730e996a355a2738cc9f251df
MikeH <mike123.henry@gmail.com>
2016-07-23 07:26:40 -0500

    removed console.log statements

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -3,17 +3,11 @@ var pathApi = require('..\\..\\server.js');
 var friendsList = require('..\\data\\friends.js');
 
 
-console.log("friendsList"+friendsList.friends[0].question[0]);
-console.log("api routes");
-
-console.log("appRoutes.appServer = "+appApi.appServer);
 
 var friend = [];
 
 appApi.appServer.post('/newFriend', function(req, res){
-    // console.log("friendsList.friends.length = "+friendsList.friends.length);
-    // console.log("req = "+req);
-    // console.log(" req.body.name =" +req.body.name);
+
     friend.push(
     {   name: req.body.name,
         photo: req.body.photo,
@@ -25,19 +19,16 @@ appApi.appServer.post('/newFriend', function(req, res){
     var compNdx = 0;
     for (var i = 0; i < friendsList.friends.length; i++) {
         compatability = 0; //reset for next loop
-        // console.log("friendsList.friends[i].question[x] = "+ friendsList.friends[1].question[1]);
-        // console.log("friends.question[x] = "+ friend[0].question[1]);
+
         for (var x = 0; x < 10; x++) {
             compatability += Math.abs(parseInt(friendsList.friends[i].question[x])-parseInt(friend[0].question[x]));
-            console.log(  "compatability = " +compatability + " i "+i +" x "+x )
         }
         if (compatability<bestCompat) {
             bestCompat = compatability;
             compNdx =i; //this is the index best match
             }
         }
-        console.log("best fit = "+friendsList.friends[compNdx].name);
-    // console.log(friends);
+
      res.json(friendsList.friends[compNdx].name);
 });
 
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -2,13 +2,6 @@ var appRoutes = require('..\\..\\server.js');
 var pathRoutes = require('..\\..\\server.js');
 
 
-console.log("html routes");
-
-console.log("appRoutes.appServer = "+appRoutes.appServer);
-
-//var path = require("path");
-
-
 appRoutes.appServer.get('/', function(req, res){
      res.sendFile(pathRoutes.pathServer.join(__dirname, '..\\public\\home.html'));
  })
