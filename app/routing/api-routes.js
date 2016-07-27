19c691356b86607732a19dfe89c179df7a1198e8
MikeH <mike123.henry@gmail.com>
2016-07-27 14:05:50 -0500

    fixed error in api routes

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -4,24 +4,31 @@ var friendsList = require('../data/friends.js');
 
 
 
-var friend = [];
+//var friend = [];
 
 appApi.appServer.post('/newFriend', function(req, res){
-
+var friend = [];
     friend.push(
     {   name: req.body.name,
         photo: req.body.photo,
-        question: [req.body.question1, req.body.question2, req.body.question3, req.body.question4, req.body.question5,
-                   req.body.question6, req.body.question7,req.body.question8, req.body.question9, req.body.question10]
+        question: [parseInt(req.body.question1),
+                    parseInt(req.body.question2),
+                    parseInt(req.body.question3),
+                    parseInt(req.body.question4),
+                    parseInt(req.body.question5),
+                    parseInt(req.body.question6),
+                    parseInt(req.body.question7),
+                    parseInt(req.body.question8),
+                    parseInt(req.body.question9),
+                    parseInt(req.body.question10)]
     });
     var compatability = 0;
     var bestCompat = 51; //initilize to bigger than the worst match
     var compNdx = 0;
     for (var i = 0; i < friendsList.friends.length; i++) {
         compatability = 0; //reset for next loop
-
         for (var x = 0; x < 10; x++) {
-            compatability += Math.abs(parseInt(friendsList.friends[i].question[x])-parseInt(friend[0].question[x]));
+            compatability += Math.abs(friendsList.friends[i].question[x]-friend[0].question[x]);
         }
         if (compatability<bestCompat) {
             bestCompat = compatability;
@@ -30,5 +37,12 @@ appApi.appServer.post('/newFriend', function(req, res){
         }
 
      res.json(friendsList.friends[compNdx].name);
+     friendsList.friends.push(friend[0]);
+     console.log(friendsList.friends)
 });
 
+
+appApi.appServer.get('/api/friends', function(req, res){
+
+    res.json(friendsList.friends);
+});
